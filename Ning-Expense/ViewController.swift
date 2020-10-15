4682d2f2845c0e5841c755cd8272a3ecd203be54
Ning Huang <nh@mega.co.nz>
2020-10-16 09:26:15 +1300

    Initial Commit

---
+++ Ning-Expense/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ning-Expense
+//
+//  Created by Ning Huang on 16/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
