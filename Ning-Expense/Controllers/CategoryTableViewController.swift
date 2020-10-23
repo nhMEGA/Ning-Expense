5c52178d1d32b0e5f345753444318e5a77177829
Ning Huang <nh@mega.co.nz>
2020-10-23 16:24:38 +1300

    feat: use code to navigate

--- Ning-Expense/Controllers/CategoryTableViewController.swift
+++ Ning-Expense/Controllers/CategoryTableViewController.swift
@@ -78,34 +78,13 @@ class CategoryTableViewController: UITableViewController {
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
     
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-        
-        if segue.identifier == "EditCategory" {
-            let destinationVC = segue.destination as! EditCategoryViewController
-            destinationVC.catIndex = tableView.indexPathForSelectedRow!.row
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let editVC = storyboard?.instantiateViewController(withIdentifier: "EditCategoryViewController") as? EditCategoryViewController else {
+            return
         }
-    }
+        editVC.catIndex = indexPath.row
 
+        navigationController?.pushViewController(editVC, animated: true)
+    }
 }
